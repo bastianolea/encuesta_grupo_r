343b3725ee1ba3d1808956707da5449f8820a230
bastianolea <bastianolea@gmail.com>
2025-11-26 23:31:31 -0300

    primera versión

---
+++ configurar.R
@@ -0,0 +1,10 @@
+library(surveydown)
+# https://surveydown.org/blog/2025-11-21-announcing-surveydown-version-1/
+  
+  
+# https://surveydown.org/docs/getting-started#start-with-a-template
+surveydown::sd_create_survey()
+
+
+# https://surveydown.org/docs/storing-data
+sd_db_config()
---
+++ respuestas.R
@@ -0,0 +1,5 @@
+library(surveydown)
+
+db <- sd_db_connect()
+
+data <- sd_get_data(db)
