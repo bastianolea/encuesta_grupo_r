f60b2434c83965abf4c08f153274f78a04d845d3
bastianolea <bastianolea@gmail.com>
2025-12-03 22:53:51 -0300

    respuestas

--- respuestas.R
+++ respuestas.R
@@ -1,5 +1,32 @@
 library(surveydown)
+library(dplyr)
 
 db <- sd_db_connect()
 
-data <- sd_get_data(db)
+data <- sd_get_data(db) |> 
+  tibble() |> 
+  arrange(desc(time_end))
+
+glimpse(data)
+
+data |> 
+  filter(!is.na(correo)) |> 
+  count(nivel)
+
+data |> 
+  filter(!is.na(correo)) |> 
+  count(frecuencia)
+
+resultados <- data |> 
+  filter(!is.na(correo)) |> 
+  select(-starts_with("time"), time_end) |> 
+  select(time_end, nombre:areas, -sector) |> 
+  mutate(time_end = lubridate::as_datetime(time_end)) |> 
+  arrange(desc(time_end))
+
+resultados
+
+resultados |> count(genero)
+
+readr::write_csv2(resultados, 
+                  "~/Downloads/resultados_grupo_r.csv")
