a844801515bcecfccbd521d75a64d2f956e5d16e
bastianolea <bastianolea@gmail.com>
2026-03-12 21:08:27 -0300

    revisar resultados

--- readme.md
+++ readme.md
@@ -1,3 +1,3 @@
-Encuestita para organizar un grupo de usuarixs de R.
+Encuestita para organizar un [grupo de usuarixs de R.](https://santiagorusers.github.io)
 
 Hecha con [`{surveydown}`](https://surveydown.org/docs/getting-started#start-with-a-template)
--- respuestas.R
+++ respuestas.R
@@ -19,14 +19,30 @@ data |>
 
 resultados <- data |> 
   filter(!is.na(correo)) |> 
-  select(-starts_with("time"), time_end) |> 
-  select(time_end, nombre:areas, -sector) |> 
-  mutate(time_end = lubridate::as_datetime(time_end)) |> 
-  arrange(desc(time_end))
+  filter(contacto == "si") |> 
+  select(-starts_with("time")) |> 
+  select(nombre:areas)
+  # select(-session_id:current_page)
+  # select(time_end, nombre:areas, -sector) |> 
+  # mutate(time_end = lubridate::as_datetime(time_end)) |> 
+  # arrange(desc(time_end))
 
-resultados
+glimpse(resultados)
+
+resultados |> count(nivel)
+resultados |> count(frecuencia)
+resultados |> count(region)
+resultados |> count(asistencia)
 
 resultados |> count(genero)
 
-readr::write_csv2(resultados, 
-                  "~/Downloads/resultados_grupo_r.csv")
+resultados |> 
+  filter(region != "fuera_de_chile") |> 
+  filter(frecuencia != "rara_vez") |> 
+  pull(correo) |> 
+  unique() |> 
+  paste(collapse = ", ") |> 
+  writeLines("~/Downloads/resultados_grupo_r.txt")
+
+# readr::write_csv2(resultados, 
+                  # "~/Downloads/resultados_grupo_r.csv")
